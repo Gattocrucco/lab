de1749d40a12a6fdedf675e4be565c9b58614410
Giacomo Petrillo <info@giacomopetrillo.com>
2016-11-15 15:43:34 +0100

    aggiunti test

--- labtest.py
+++ labtest.py
@@ -4,9 +4,18 @@ import lab
 class TestFormatting(unittest.TestCase):
 
 	def test_num2si(self):
+		# generic check
 		self.assertEqual(lab.num2si(1312), '1.312 k')
+		# check crash on 0
 		self.assertEqual(lab.num2si(0), '0')
+		# check that format options are respected
 		self.assertEqual(lab.num2si(1, format='%+g'), '+1')
+	
+	def test_util_format(self):
+		# check that big-exponent values use exponential notation
+		self.assertEqual(lab.util_format(1.23456789e-8, 1.1111e-10, pm=None, percent=False), '1.235(11)e-8')
+		# check that number of digits is chosen correctly in case of uncertainty rounding
+		self.assertEqual(lab.util_format(10, 0.99, pm=None, percent=False), '10.0(10)')
 
 	# def test_isupper(self):
 	# 	self.assertTrue('FOO'.isupper())
