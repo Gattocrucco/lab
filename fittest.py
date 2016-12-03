9511dec0afcd80abc812264f1a0f8b066d1c89f9
Giacomo Petrillo <info@giacomopetrillo.com>
2016-12-03 01:28:03 +0100

    completato fittest plot parametri vs. errori media pesata (con switch)

--- fittest.py
+++ fittest.py
@@ -9,27 +9,33 @@ from strangefit import *
 from scipy.optimize import curve_fit
 import os
 
+# TODO
+# usare su tutti i subplot sigma, percentual e mean distance
+
 #### PARAMETERS ####
-showplot = False # show plot after monte carlo run with fixed parameters
-showpsplot = True # show parameter biases with fixed data
+showplot = True # show plot after monte carlo run with fixed parameters
+showpsplot = False # show parameter biases with fixed data
 showpsdtplot = False # show parameter vs. errors
 stattest = False # perform statistical test after monte carlo
+weightedaverage = True
 p0s = [ # true parameters, axis 0 = parameter, axis 1 = values
-	linspace(-1,1,20),
-	logspace(0,1,20),
+	# linspace(-1,1,10),
+	# logspace(0,1,10),
+	[1],
+	[1]
 ]
 fs = [ # sympy functions
 	lambda x, a, b: a * sp.exp(x / b),
 	lambda x, m, q: m * x + q,
 	lambda x, m: m * x
 ]
-f = fs[0] # function to fit
-mcn = 100 # number of repetitions (monte carlo)
-fitfun = 'ev' # ev, odr, odrpack, curve_fit, hoch
+f = fs[1] # function to fit
+mcn = 1000 # number of repetitions (monte carlo)
+fitfun = 'odrpack' # ev, odr, odrpack, curve_fit, hoch
 xmean = linspace(0, 3, 100) # true x
 n = len(xmean) # number of points
 dys = outer([1], ones(n)*.1) # errors, axis 0 = dataset, axis 1 = point
-dxs = outer([1], ones(n)*.1)
+dxs = outer([10], ones(n)*.1)
 ####################
 
 # initialize symbols
@@ -39,24 +45,25 @@ syms = [xsym] + psym
 
 # format function in LaTeX and 1D text
 flatex = sp.latex(f(*syms))
-subscr = '₀₁₂₃₄₅₆₇₈₉'
-psubsym = [sp.Symbol('p%c' % subscr[i], real=True) for i in range(len(p0s))]
-fstr = str(f(xsym, *psubsym)).replace('*', '·')
+psubsym = [sp.Symbol('p%s' % lab.num2sub(i), real=True) for i in range(len(p0s))]
+fstr = str(f(xsym, *psubsym)).replace('**', '^').replace('*', '·')
 
 # compute derivatives and lambdify
-dfdx = sp.lambdify(syms, f(*syms).diff(xsym), "numpy")
-dfdps = [sp.lambdify(syms, f(*syms).diff(psym[i]), "numpy") for i in range(len(psym))]
-dfdp_rt = empty((len(p0s), len(xmean)))
-def dfdp(x, *p):
-	for i in range(len(p)):
-		dfdp_rt[i] = dfdps[i](x, *p)
-	return dfdp_rt
-dfdpdxs = [sp.lambdify(syms, f(*syms).diff(xsym).diff(psym[i]), "numpy") for i in range(len(psym))]
-dfdpdx_rt = empty((len(p0s), len(xmean)))
-def dfdpdx(x, *p):
-	for i in range(len(p)):
-		dfdpdx_rt[i] = dfdpdxs[i](x, *p)
-	return dfdpdx_rt
+# dfdx = sp.lambdify(syms, f(*syms).diff(xsym), "numpy")
+# dfdps = [sp.lambdify(syms, f(*syms).diff(psym[i]), "numpy") for i in range(len(psym))]
+# dfdp_rt = empty((len(p0s), len(xmean)))
+# def dfdp(x, *p):
+# 	for i in range(len(p)):
+# 		dfdp_rt[i] = dfdps[i](x, *p)
+# 	return dfdp_rt
+# dfdpdxs = [sp.lambdify(syms, f(*syms).diff(xsym).diff(psym[i]), "numpy") for i in range(len(psym))]
+# dfdpdx_rt = empty((len(p0s), len(xmean)))
+# def dfdpdx(x, *p):
+# 	for i in range(len(p)):
+# 		dfdpdx_rt[i] = dfdpdxs[i](x, *p)
+# 	return dfdpdx_rt
+
+model = lab.FitModel(f)
 
 fsym = f
 f = sp.lambdify(syms, f(*syms), "numpy")
@@ -103,31 +110,43 @@ for ll in range(len(dys)):
 				# fit
 				start = time.time()
 				if fitfun == 'ev':
-					par, cov = lab.fit_generic_xyerr(f, dfdx, x, y, dx, dy, p0=p0, max_cycles=10)
+					par, cov = lab.fit_generic(model, x, y, dx, dy, p0=p0, method='ev', max_cycles=10)
 				elif fitfun == 'odr':
-					par, cov = lab.fit_generic_xyerr3(f, dfdx, dfdp, dfdpdx, x, y, dx, dy, p0)
+					par, cov = lab.fit_generic(model, x, y, dx, dy, p0=p0, method='linodr')
 				elif fitfun == 'hoch':
-					par, cov = fit_generic_xyerr4(f, fi, dfdp, dfidp, x, y, dx, dy, p0)
+					par, cov = fit_generic_xyerr4(model.f(), model.fi(), model.dfdp_odrpack(), model.dfidp(), x, y, dx, dy, p0)
 				elif fitfun == 'odrpack':
-					par, cov = lab.fit_generic_xyerr2(f, dfdx, dfdp, x, y, dx, dy, p0=p0)
+					par, cov = lab.fit_generic(model, x, y, dx, dy, p0=p0, method='odrpack')
 				elif fitfun == 'curve_fit':
-					par, cov = curve_fit(f, x, y, sigma=dy, p0=p0, absolute_sigma=True, jac=lambda *args: dfdp(*args).T)
+					par, cov = curve_fit(f, x, y, sigma=dy, p0=p0, absolute_sigma=True, jac=model.dfdp_curve_fit(len(x)))
 				end = time.time()
 				
 				# save results
 				if showplot or stattest:
 					times[i] = end - start
-					chisq[i] = ((y - f(x, *par))**2 / (dy**2 + (dfdx(x, *par)*dx)**2)).sum()
+					chisq[i] = ((y - model.f()(x, *par))**2 / (dy**2 + (model.dfdx()(x, *par)*dx)**2)).sum()
 				pars[i] = par
 				covs[i] = cov
 			
 			# save results
-			pm = pars.mean(axis=0)
-			pc = empty(2 * [len(p0)])
-			for i in range(len(p0)):
-				for j in range(len(p0)):
-					pc[i, j] = ((pars[:,i] - pm[i]) * (pars[:,j] - pm[j])).sum() / len(pars)
-			ps = sqrt(diag(pc))
+			if weightedaverage:
+				icovs = empty(covs.shape)
+				for i in range(len(icovs)):
+					icovs[i] = np.linalg.inv(covs[i])
+				pc = np.linalg.inv(icovs.sum(axis=0))
+				wpar = empty(pars.shape)
+				for i in range(len(wpar)):
+					wpar[i] = icovs[i].dot(pars[i])
+				pm = pc.dot(wpar.sum(axis=0))
+				pc *= len(pars)
+				ps = sqrt(diag(pc))
+			else:
+				pm = pars.mean(axis=0)
+				pc = empty(2 * [len(p0)])
+				for i in range(len(p0)):
+					for j in range(len(p0)):
+						pc[i, j] = ((pars[:,i] - pm[i]) * (pars[:,j] - pm[j])).sum() / len(pars)
+				ps = sqrt(diag(pc))
 
 			fp[(l, ll) + K] = pm
 			cp[(l, ll) + K] = pc / len(pars)
@@ -139,6 +158,14 @@ for ll in range(len(dys)):
 				for i in range(len(p0)):
 					for j in range(len(p0)):
 						fc[i,j] = covs[:,i,j].mean()
+				frho = empty(2 * [len(p0)])
+				for i in range(len(p0)):
+					for j in range(len(p0)):
+						frho[i,j] = (covs[:,i,j] / sqrt(covs[:,i,i] * covs[:,j,j])).mean()
+				prho = empty(2 * [len(p0)])
+				for i in range(len(p0)):
+					for j in range(len(p0)):
+						prho[i,j] = pc[i,j] / sqrt(pc[i,i] * pc[j,j])
 
 				pdist = (pm - array(p0)) / ps
 				chidist = (chisq.mean() - (n-len(p0))) / chisq.std(ddof=1)
@@ -159,7 +186,7 @@ for ll in range(len(dys)):
 			
 			if showplot:
 				
-				figure('Fit with method “%s”, function %s' % (fitfun, fstr), figsize=(14,10)).set_tight_layout(True)
+				figure('Function %s, fit with method “%s”' % (fstr, fitfun), figsize=(14,10)).set_tight_layout(True)
 				clf()
 				nbins = int(sqrt(min(mcn, 1000)))
 				maxscatter = 1000
@@ -202,8 +229,8 @@ for ll in range(len(dys)):
 					for j in range(i):
 						subplot(rows, cols, 2 + i * cols + j)
 						title('$\\rho_{%d%d}\'-\\rho_{%d%d}$' % (i, j, i, j))
-						plot_text('True $\\rho =$ %.2g\nDistance = %.2g %%' % (pc[i,j] / ps[i] / ps[j], 100*(fc[i,j] - pc[i,j]) / pc[i,j]))
-						hist(covs[:,i,j] / sqrt(covs[:,i,i]*covs[:,j,j]) - pc[i,j] / ps[i] / ps[j], bins=nbins, color=hcolor)
+						plot_text('True $\\rho =$ %.2g\nDistance = %.2g %%' % (prho[i,j], 100*(frho[i,j] - prho[i,j]) / prho[i,j]))
+						hist(covs[:,i,j] / sqrt(covs[:,i,i]*covs[:,j,j]) - prho[i,j], bins=nbins, color=hcolor)
 						ticklabel_format(style='sci', axis='x', scilimits=(-3,3))
 				
 				# histogram of chisquare; last row column 1
@@ -222,22 +249,18 @@ for ll in range(len(dys)):
 				subplot(rows, cols, rows * cols)
 				title('Example fit')
 				errorbar(x, y, dy, dx, fmt=',k', capsize=0, label='Data')
-				fx = linspace(min(x), max(x), 1000)
-				plot(fx, f(fx, *par), 'r-', label='$y=%s$' % flatex)
+				fx = linspace(min(xmean), max(xmean), 1000)
+				plot(fx, f(fx, *p0), 'r-', label='$y=%s$' % flatex)
 				# plot_text('$y=%s$\n$y=%s$' % (flatex, sp.latex(fsym(xsym, *p0))), fontsize=20)
 				ticklabel_format(style='sci', axis='both', scilimits=(-3,3))
 				legend(loc=0)
 				
 				# save figure and show
-				filename = gcf().canvas.get_window_title().replace('/', '∕')
-				i = 1
-				while os.path.exists('%s-%02d.pdf' % (filename, i)):
-					i += 1
-				savefig('%s-%02d.pdf' % (filename, i))
+				savefig(lab.nextfilename(gcf().canvas.get_window_title(), '.pdf'))
 				show()
 
 if showpsplot:
-	figure('Fit with method “%s”, function %s, parameter biases' % (fitfun, fstr), figsize=(14,10)).set_tight_layout(True)
+	figure('Function %s, fit with method “%s”, parameter biases' % (fstr, fitfun), figsize=(14,10)).set_tight_layout(True)
 	clf()
 	for i in range(len(p0s)): # p_i = fitted
 		for j in range(len(p0s)): # p_j = true
@@ -256,11 +279,7 @@ if showpsplot:
 			ticklabel_format(style='sci', axis='both', scilimits=(-3,3))
 			grid()
 				
-	filename = gcf().canvas.get_window_title().replace('/', '∕')
-	i = 1
-	while os.path.exists('%s-%02d.pdf' % (filename, i)):
-		i += 1
-	savefig('%s-%02d.pdf' % (filename, i))
+	savefig(lab.nextfilename(gcf().canvas.get_window_title(), '.pdf'))
 	show()
 	# figure('Slope 3d, fit with method “%s”' % fitfun).set_tight_layout(True)
 	# clf()
@@ -272,31 +291,33 @@ if showpsplot:
 	# gca().set_zlabel('$m\'-m$')
 
 if showpsdtplot:
-	figure('Slope vs dx, %s' % fitfun).set_tight_layout(True)
+	
+	figure('Function %s, fit with method “%s”, parameters vs. errors' % (fstr, fitfun), figsize=(14,10)).set_tight_layout(True)
 	clf()
-	subplot(211)
-	title('True m = %g, q = %g, sum(dy^2) = %g' % (ms[0], qs[0], (dys[0]**2).sum()))
-	xlabel('sum of dx**2')
-	ylabel('Fitted m')
-	errorbar((dxs**2).sum(axis=-1), fp[0,0,:,0,0], dp[0,0,:,0,0], fmt=',')
-	subplot(212)
-	title('True m = %g, q = %g, sum(dx^2) = %g' % (ms[0], qs[0], (dxs[0]**2).sum()))
-	xlabel('sum of dy**2')
-	ylabel('Fitted m')
-	errorbar((dys**2).sum(axis=-1), fp[0,0,0,:,0], dp[0,0,0,:,0], fmt=',')
-
-
-	figure('Offset vs dx, %s' % fitfun).set_tight_layout(True)
-	clf()
-	subplot(211)
-	title('True m = %g, q = %g, sum(dy^2) = %g' % (ms[0], qs[0], (dys[0]**2).sum()))
-	xlabel('sum of dx**2')
-	ylabel('Fitted q')
-	errorbar((dxs**2).sum(axis=-1), fp[0,0,:,0,1], dp[0,0,:,0,1], fmt=',')
-	subplot(212)
-	title('True m = %g, q = %g, sum(dx^2) = %g' % (ms[0], qs[0], (dxs[0]**2).sum()))
-	xlabel('sum of dy**2')
-	ylabel('Fitted q')
-	errorbar((dys**2).sum(axis=-1), fp[0,0,0,:,1], dp[0,0,0,:,1], fmt=',')
-
+	
+	ds = [
+		[dxs, dys, 'x', 'y', (Ellipsis, 0)],
+		[dys, dxs, 'y', 'x', (0, Ellipsis)]
+	]
+	for i in range(len(p0s)):
+		for j in range(2):
+			subplot(len(p0s), 2, 2*i + j + 1)
+			if i == 0:
+				pstr = ''
+				for k in range(len(p0s)):
+					pstr += '$p_{%d}$ = %.2g\n' % (k, p0s[k][0])
+				pstr += '$\sqrt{\sum\Delta %s^2/n}=$%.2g' % (ds[j][3], sqrt((ds[j][1][0]**2).sum() / n))
+				plot_text(pstr)
+			if i == len(p0s) - 1:
+				xlabel('$\sqrt{\sum\Delta %s^2/n}$' % ds[j][2])
+			if j == 0:
+				ylabel('$p_{%d}\'-p_{%d}$' % (i, i))
+			sel = ds[j][4] + tuple([0] * len(p0s)) + (i,)
+			Y = fp[sel] - asarray(p0s[i])
+			DY = sqrt(cp[sel + (i,)])
+			errorbar(sqrt((ds[j][0]**2).sum(axis=-1) / n), Y, DY, fmt=',')
+			pvalue = st.chi2.sf(sum((Y / DY)**2), len(Y))
+			plot_text('p-value = %.2g %%' % (pvalue * 100), loc=1)
+		
+	savefig(lab.nextfilename(gcf().canvas.get_window_title(), '.pdf'))
 	show()
