b73a5223feee91dba43f0565f972db4afd2d521b
Giacomo Petrillo <info@giacomopetrillo.com>
2017-07-23 23:37:20 +0200

    modified fittest

--- fittest.py
+++ fittest.py
@@ -12,7 +12,7 @@ p0s = [ # true parameters, axis 0 = parameter, axis 1 = values
 	# logspace(0,1,10),
 	[1],
 	[2],
-	[3],
+	# [3],
 ]
 fs = [ # sympy functions
 	lambda x, a, b: a * sympy.exp(x / b),
@@ -20,14 +20,14 @@ fs = [ # sympy functions
 	lambda x, m: m * x,
 	lambda t, A, w, phi: A * sympy.sin(w * t + phi)
 ]
-f = fs[3] # function to fit
+f = fs[1] # function to fit
 
-mcn = 100 # number of repetitions (monte carlo)
-methods = ['linodr2', 'linodr2bis', 'linodr2nd'] # ev, linodr, odrpack, ml, wleastsq, leastsq
+mcn = 1000 # number of repetitions (monte carlo)
+methods = ['odrpack', 'linodr2', 'linodr2bis'] # ev, linodr, odrpack, ml, wleastsq, leastsq
 xmean = np.linspace(0, 10, 10) # true x
 n = len(xmean) # number of points
 dys = np.outer([1], np.ones(n)*.1) # errors, axis 0 = dataset, axis 1 = point
-dxs = np.outer([1], np.ones(n)*.1)
+dxs = np.outer([3], np.ones(n)*.1)
 ####################
 
 method_kw = []
@@ -36,7 +36,7 @@ for m in methods:
 
 model = CurveModel(f, symb=True)
 plot = dict(single=showplot, vsp0=showpsplot, vsds=showpsdtplot)
-out = fit_curve_bootstrap(model, xmean, dxs=dxs, dys=dys, p0s=p0s, mcn=mcn, method=methods, plot=plot, eta=True, wavg=False, method_kw=method_kw)
+out = fit_curve_bootstrap(model, xmean, dxs=dxs, dys=dys, p0s=p0s, mcn=mcn, method=methods, plot=plot, eta=True, wavg=True, method_kw=method_kw)
 
 figs = []
 if showplot:
